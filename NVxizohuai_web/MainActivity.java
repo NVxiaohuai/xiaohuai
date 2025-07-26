d9ab198ae079f73da9b516f7adda2006d0ac23e2
xh <3382606608@qq.com>
2025-07-26 11:40:16 +0800

    Add files via upload

---
+++ NVxizohuai_web/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.nvxizohuai;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // 启用JavaScript
+
+        // 加载本地HTML文件
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
